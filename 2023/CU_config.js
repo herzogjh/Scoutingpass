5f09eb668bd84c757a97abd33289f3c2b9b3aa89
Jason Herzog <herzogjh@gmail.com>
2023-11-19 19:18:06 -0600

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -1,194 +1,192 @@
 var config_data = `
 {
-  
-    "dataFormat": "kvs",
-    "title": "MinneTrials 2023",
-    "page_title": "Corn Conundrum",
-  main
-    "checkboxAs": "10",
-    "prematch": [
-      { "name": "Scouter Initials",
-        "code": "s",
-        "type": "scouter",
-        "size": 5,
-        "maxSize": 5,
-        "required": "true"
+  "dataFormat": "kvs",
+  "title": "MinneTrials 2023",
+  "page_title": "Corn Conundrum",
+  "checkboxAs": "10",
+  "prematch": [
+    { "name": "Scouter Initials",
+      "code": "s",
+      "type": "scouter",
+      "size": 5,
+      "maxSize": 5,
+      "required": "true"
+    },
+    { "name": "Event",
+      "code": "e",
+      "type": "event",
+      "defaultValue": "MT23",
+      "required": "true",
+      "disabled": "true"
+    },
+    { "name": "Match Level",
+      "code": "l",
+      "type": "level",
+      "choices": {
+        "qm": "Quals<br>",
+        "sf": "Semifinals<br>",
+        "f": "Finals"
       },
-      { "name": "Event",
-        "code": "e",
-        "type": "event",
-        "defaultValue": "MT23",
-        "required": "true",
-        "disabled": "true"
+      "defaultValue": "qm",
+      "required": "true"
+    },
+    { "name": "Match #",
+      "code": "m",
+      "type": "match",
+      "min": 1,
+      "max": 100,
+      "required": "true"
+    },
+    { "name": "Robot",
+      "code": "r",
+      "type": "robot",
+      "choices": {
+        "r1": "Red-1",
+        "b1": "Blue-1<br>",
+        "r2": "Red-2",
+        "b2": "Blue-2<br>",
       },
-      { "name": "Match Level",
-        "code": "l",
-        "type": "level",
-        "choices": {
-          "qm": "Quals<br>",
-          "sf": "Semifinals<br>",
-          "f": "Finals"
-        },
-        "defaultValue": "qm",
-        "required": "true"
+      "required":"true"
+    },
+    { "name": "Team #",
+      "code": "t",
+      "type": "team",
+      "min": 1,
+      "max": 99999
+    },
+    { "name": "Auto Start Position",
+      "code": "as",
+      "type": "clickable_image",
+      "filename": "2023/field_image.png",
+      "clickRestriction": "one",
+      "shape": "circle 5 black red true"
+    }
+  ],
+  "auton": [
+    { "name": "Game Pieces attempted<br>(Scored and Missed)",
+      "code": "aa",
+      "type": "counter"
+    },
+    { "name": "Exited BARN ZONE?",
+      "code": "am",
+      "type": "bool"
+    },
+      "defaultValue": "x"
+    }
+  ],
+  "teleop": [
+    { "name": "Feeder Count<br>(Fed another bot)",
+      "code": "tfc",
+      "type": "counter"
+    },
+    { "name": "Was Fed<br>Game Pieces",
+      "code": "wf",
+      "type": "bool"
+    },
+    { "name": "Was Defended",
+      "code": "wd",
+      "type": "bool"
+    },
+    { "name": "Who Defended this bot",
+      "code": "who",
+      "type": "text"
+    },
+    { "name": "Smart Placement<br>(creates Links)",
+      "code": "lnk",
+      "type": "bool"
+    },
+    { "name": "Floor Pickup",
+      "code": "fpu",
+      "type": "bool",
       },
-      { "name": "Match #",
-        "code": "m",
-        "type": "match",
-        "min": 1,
-        "max": 100,
-        "required": "true"
+      "defaultValue": "x"
+    },
+    { "name": "GRAIN ELEVATOR Used?",
+      "code": "sub",
+      "type": "bool",
       },
-      { "name": "Robot",
-        "code": "r",
-        "type": "robot",
-        "choices": {
-          "r1": "Red-1",
-          "b1": "Blue-1<br>",
-          "r2": "Red-2",
-          "b2": "Blue-2<br>",
-        },
-        "required":"true"
+      "defaultValue": "x"
+    }
+  ],
+  "endgame": [
+    { "name": "Movement towards BARN ZONE Timer",
+      "code": "dt",
+      "type": "timer"
+    },
+    { "name": "Final Status",
+      "code": "fs",
+      "type":"radio",
+      "choices": {
+        "p": "Parked<br>",
+        "s": "Parked and brought SPOILED COB<br>",
+        "a": "Attempted<br>",
+        "x": "Not attempted"
       },
-      { "name": "Team #",
-        "code": "t",
-        "type": "team",
-        "min": 1,
-        "max": 99999
+      "defaultValue": "x"
+    },
+    { "name": "Total # of alliance<br>robots docked/engaged",
+      "code": "dn",
+      "type": "counter"
+    },
+  ],
+  "postmatch": [
+    { "name": "Driver Skill",
+      "code": "ds",
+      "type": "radio",
+      "choices": {
+        "n": "Not Effective<br>",
+        "a": "Average<br>",
+        "v": "Very Effective<br>",
+        "x": "Not Observed"
       },
-      { "name": "Auto Start Position",
-        "code": "as",
-        "type": "clickable_image",
-        "filename": "2023/field_image.png",
-        "clickRestriction": "one",
-        "shape": "circle 5 black red true"
-      }
-    ],
-    "auton": [
-      { "name": "Game Pieces attempted<br>(Scored and Missed)",
-        "code": "aa",
-        "type": "counter"
+      "defaultValue": "x"
+    },
+    { "name": "Defense Rating",
+      "code": "dr",
+      "type": "radio",
+      "choices": {
+        "b": "Below Average<br>",
+        "a": "Average<br>",
+        "g": "Good<br>",
+        "e": "Excellent<br>",
+        "x": "Did not play defense"
       },
-      { "name": "Exited BARN ZONE?",
-        "code": "am",
-        "type": "bool"
+      "defaultValue": "x"
+    },
+    { "name": "Speed Rating",
+      "code": "sr",
+      "type": "radio",
+      "choices": {
+        "1": "1 (slow)<br>",
+        "2": "2<br>",
+        "3": "3<br>",
+        "4": "4<br>",
+        "5": "5 (fast)"
       },
-        "defaultValue": "x"
-      }
-    ],
-    "teleop": [
-      { "name": "Feeder Count<br>(Fed another bot)",
-        "code": "tfc",
-        "type": "counter"
-      },
-      { "name": "Was Fed<br>Game Pieces",
-        "code": "wf",
-        "type": "bool"
-      },
-      { "name": "Was Defended",
-        "code": "wd",
-        "type": "bool"
-      },
-      { "name": "Who Defended this bot",
-        "code": "who",
-        "type": "text"
-      },
-      { "name": "Smart Placement<br>(creates Links)",
-        "code": "lnk",
-        "type": "bool"
-      },
-      { "name": "Floor Pickup",
-        "code": "fpu",
-        "type": "bool",
-        },
-        "defaultValue": "x"
-      },
-      { "name": "GRAIN ELEVATOR Used?",
-        "code": "sub",
-        "type": "bool",
-        },
-        "defaultValue": "x"
-      }
-    ],
-    "endgame": [
-      { "name": "Movement towards BARN ZONE Timer",
-        "code": "dt",
-        "type": "timer"
-      },
-      { "name": "Final Status",
-        "code": "fs",
-        "type":"radio",
-        "choices": {
-          "p": "Parked<br>",
-          "s": "Parked and brought SPOILED COB<br>",
-          "a": "Attempted<br>",
-          "x": "Not attempted"
-        },
-        "defaultValue": "x"
-      },
-      { "name": "Total # of alliance<br>robots docked/engaged",
-        "code": "dn",
-        "type": "counter"
-      },
-    ],
-    "postmatch": [
-      { "name": "Driver Skill",
-        "code": "ds",
-        "type": "radio",
-        "choices": {
-          "n": "Not Effective<br>",
-          "a": "Average<br>",
-          "v": "Very Effective<br>",
-          "x": "Not Observed"
-        },
-        "defaultValue": "x"
-      },
-      { "name": "Defense Rating",
-        "code": "dr",
-        "type": "radio",
-        "choices": {
-          "b": "Below Average<br>",
-          "a": "Average<br>",
-          "g": "Good<br>",
-          "e": "Excellent<br>",
-          "x": "Did not play defense"
-        },
-        "defaultValue": "x"
-      },
-      { "name": "Speed Rating",
-        "code": "sr",
-        "type": "radio",
-        "choices": {
-          "1": "1 (slow)<br>",
-          "2": "2<br>",
-          "3": "3<br>",
-          "4": "4<br>",
-          "5": "5 (fast)"
-        },
-        "defaultValue":"3"
-      },
-      { "name": "Died/Immobilized",
-        "code": "die",
-        "type": "bool"
-      },
-      { "name": "Tippy<br>(almost tipped over)",
-        "code": "tip",
-        "type": "bool"
-      },
-      { "name": "Dropped CORN COBS (>2)",
-        "code": "dc",
-        "type": "bool"
-      },
-      { "name": "Make good<br>alliance partner?",
-        "tooltip": "Would you want this robot on your alliance in eliminations?",
-        "code": "all",
-        "type": "bool"
-      },
-      { "name": "Comments",
-        "code": "co",
-        "type": "text",
-        "size": 15,
-        "maxSize": 55
-      }
-    ]
+      "defaultValue":"3"
+    },
+    { "name": "Died/Immobilized",
+      "code": "die",
+      "type": "bool"
+    },
+    { "name": "Tippy<br>(almost tipped over)",
+      "code": "tip",
+      "type": "bool"
+    },
+    { "name": "Dropped CORN COBS (>2)",
+      "code": "dc",
+      "type": "bool"
+    },
+    { "name": "Make good<br>alliance partner?",
+      "tooltip": "Would you want this robot on your alliance in eliminations?",
+      "code": "all",
+      "type": "bool"
+    },
+    { "name": "Comments",
+      "code": "co",
+      "type": "text",
+      "size": 15,
+      "maxSize": 55
+    }
+  ]
 }`;
